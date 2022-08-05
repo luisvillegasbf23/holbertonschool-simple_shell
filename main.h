17dce7459181e72bd083fce53a7e8d0f0cf69f91
luisvillegasbf23 <luisville.lv@gmail.com>
2022-08-05 10:49:22 -0300

    commit

--- main.h
+++ main.h
@@ -1,10 +1,10 @@
-#ifndef SIMPLESHELL_H 
+#ifndef SIMPLESHELL_H
 #define SIMPLESHELL_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>                                                 
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 extern char **environ;
